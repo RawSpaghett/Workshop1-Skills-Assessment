49fa481171d46356e518ead848a4e81663f2d306
RawSpaghett <rawspaghetti05@gmail.com>
2026-09-01 12:00:31 -0400

    O and P UI requirements

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float gravity = -9.8f; //earth
+
+    //Movement  
+    private CharacterController controller;
+    private Vector3 moveInput;
+    private Vector3 velocity;
+
+    //UI
+    [SerializeField] private GameObject OText;
+    [SerializeField] private GameObject PText;
+    [SerializeField] private GameObject UICanvas;
+    private bool UISwitch = false;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+        Debug.Log($"Move Input: {moveInput}");
+    }
+
+    public void OnPopup(InputAction.CallbackContext context)
+    {
+        if(context.started)
+        {
+            if(UICanvas.activeSelf) UICanvas.SetActive(false);
+            else UICanvas.SetActive(true);
+            
+            if(context.control.name == "o")
+            {
+                PText.SetActive(false);
+                OText.SetActive(true);
+            }
+            if (context.control.name == "p")
+            {
+                OText.SetActive(false);
+                PText.SetActive(true);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 move = new Vector3(moveInput.x,0,moveInput.y);
+        controller.Move(move * speed * Time.deltaTime);
+    }
+}
